e66d793c9fe7899d38435f0f2ccae3ad4d0bb10a
Miguel Diaz <diazmiiguel@gmail.com>
2022-08-28 15:30:06 -0500

    changes flag names for redis and zedql

--- app/Main.hs
+++ app/Main.hs
@@ -20,7 +20,7 @@ import Weather (weatherFrom)
 default (Text)
 
 data Fishy
-  = Mix {project :: FilePath, testPath :: Maybe Text, withRedis :: Bool, isZedQL :: Bool}
+  = Mix {project :: FilePath, testPath :: Maybe Text, redis :: Bool, zedQL :: Bool}
   | Weather {cityName :: Text}
   deriving (Show, Data, Typeable)
 
@@ -33,8 +33,8 @@ elixirSuite =
   Mix
     { project = def &= help "Path to Elixir project",
       testPath = def &= help "Optional path to the test file",
-      withRedis = def &= help "Whether to spawn a local Redis instance",
-      isZedQL = def &= help "Whether or not the current project is ZED QL so we can run other migrations first"
+      redis = def &= help "Whether to spawn a local Redis instance",
+      zedQL = def &= help "Whether or not the current project is ZED QL so we can run other migrations first"
     }
 
 weather :: Fishy
@@ -84,10 +84,10 @@ closeRedisServer = do
 
 handleElixirSuite :: Fishy -> Maybe Text -> Sh ()
 handleElixirSuite cArgs home = do
-  when (withRedis cArgs) $ do
+  when (redis cArgs) $ do
     void $ asyncSh runRedisServer
 
-  when (isZedQL cArgs) $ do
+  when (zedQL cArgs) $ do
     let (racing, zedApi) = ("racing-api", "zed-api")
     void $ runElixirMigrations (elixirProjectPath home zedApi) zedApi
     void $ runElixirMigrations (elixirProjectPath home racing) racing
@@ -98,7 +98,7 @@ handleElixirSuite cArgs home = do
 
   -- The redis server is started async and stays in the background so
   -- we're explicitly shutting it down
-  when (withRedis cArgs) closeRedisServer
+  when (redis cArgs) closeRedisServer
 
   exit suiteExitCode
 
